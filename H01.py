dd42d5c7da8414072709e81d7d6d9b941ba05891
martenki <marten.kiisa@gmail.com>
2025-10-09 14:42:07 +0300

    Add files via upload

--- H01.py
+++ H01.py
@@ -1,1 +1,45 @@
-print("Tere Maailm")
+import turtle
+# turtle.shape("turtle")
+
+#kolmnurk
+# turtle.penup()
+# turtle.goto(-400,200)
+# turtle.pendown()
+# for i in range (3):
+#     turtle.fd(200)
+#     turtle.lt(120)
+# 
+# #ruut
+# turtle.penup()
+# turtle.goto(-400,0)
+# turtle.pendown()
+# for i in range (4):
+#     turtle.fd(150)
+#     turtle.lt(90)
+
+#M word
+turtle.fd(50)
+turtle.lt(90)
+turtle.fd(200)
+turtle.lt(90)
+turtle.fd(50)
+turtle.lt(40)
+turtle.fd(40)
+turtle.rt(80)
+turtle.fd(40)
+turtle.lt(40)
+turtle.fd(50)
+turtle.lt(90)
+turtle.fd(200)
+turtle.lt(90)
+turtle.fd(50)
+turtle.lt(90)
+turtle.fd(120)
+turtle.rt(120)
+turtle.fd(35)
+turtle.lt(60)
+turtle.fd(35)
+turtle.rt(120)
+turtle.fd(120)
+          
+turtle.done()
